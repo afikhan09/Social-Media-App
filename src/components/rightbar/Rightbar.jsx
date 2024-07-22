c82c871ddad2c6ccfbddbaa1d2f1c85dc64c650d
afikhan.javaventures@gmail.com <Afikhan123$>
2024-07-23 03:27:09 +0530

    implementing rightBar UI

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,7 +1,31 @@
 import "./rightbar.css";
 
 const Rightbar = () => {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarwrapper">
+        <div className="birthdayContainer">
+          <img src="/assets/gift.png" alt="" className="gifImg" />
+          <span>
+            <b>Pola Foster</b> and <b>3 other friends</b> have birthday today
+          </span>
+        </div>
+
+        <img className="rightbarAd" src="/assets/ad.png" alt="" />
+
+        <h4>Online Friends</h4>
+
+        <div className="onlinefriends">
+          <img
+            className="onlinefriendsPic"
+            src="/assets/person/1.jpeg"
+            alt=""
+          />
+          <span>Safak kocago</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Rightbar;
